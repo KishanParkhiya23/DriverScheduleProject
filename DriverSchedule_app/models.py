31058812b71132ae5eebed997cfb329c885fd479
Jay <jaymangukiya@gmail.com>
2023-08-25 09:55:22 +0530

    Driver FOrm Completed

--- DriverSchedule_app/models.py
+++ DriverSchedule_app/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 # def generate_4digit_unique_key():
-#     try:  
+#     try:
 #         latest_data = Driver.objects.latest('Uniqeid')
-#         latest_data = int(latest_data.Uniqeid)  
+#         latest_data = int(latest_data.Uniqeid)
 #         random = latest_data+1
 #     except:
 #         random =  1111
 #     return random
+
 
 class Driver(models.Model):
     # driverId = models.IntegerField(primary_key=True, unique=True, default=generate_4digit_unique_key, editable=False)
@@ -20,23 +21,34 @@ class Driver(models.Model):
     def __str__(self) -> str:
         return str(self.driverId)
 
+
 class Client(models.Model):
     clientId = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
 
     def __str__(self) -> str:
-        return  str(self.name)
+        return str(self.name)
+
+
+class Source(models.Model):
+
+    sourceName = models.CharField(primary_key=True, max_length=200)
+
+    def __str__(self) -> str:
+        return str(self.sourceName)
 
 
 class Trip(models.Model):
     driverId = models.ForeignKey(Driver, on_delete=models.CASCADE)
     clientName = models.CharField(max_length=200)
-    truckNo = models.IntegerField()
     shiftType = models.CharField(max_length=200)
-    startDateTime = models.DateTimeField()
-    endDateTime = models.DateTimeField()
-    numberOfLoad = models.IntegerField()
-    loadSheet = models.CharField(max_length=200)
+    numberOfLog = models.IntegerField()
+    truckNo = models.IntegerField()
+    shiftDate = models.DateTimeField()
+    source = models.ForeignKey(Source, on_delete=models.PROTECT)
+    startTime = models.CharField(max_length=200)
+    endTime = models.CharField(max_length=200)
+    logSheet = models.CharField(max_length=200)
     comment = models.CharField(max_length=200)
 
     def __str__(self) -> str:
@@ -50,19 +62,22 @@ class Docket(models.Model):
     docketFile = models.CharField(max_length=200)
 
     def __str__(self) -> str:
-        return  str(self.tripId)
+        return str(self.tripId)
+
 
 class AdminTruck(models.Model):
-    adminTruckNumber = models.PositiveIntegerField(validators=[MaxValueValidator(99999),MinValueValidator(10000)], unique=True)
-    def __str__(self):
-        return  str(self.adminTruckNumber)
+    adminTruckNumber = models.PositiveIntegerField(
+        validators=[MaxValueValidator(99999), MinValueValidator(10000)], unique=True)
     
+    def __str__(self):
+        return str(self.adminTruckNumber)
+
 
 class ClientTruckConnection(models.Model):
-    truckNumber = models.ForeignKey(AdminTruck, on_delete=models.CASCADE)    
-    clientId = models.ForeignKey(Client, on_delete=models.CASCADE)    
-    clientTruckId = models.PositiveIntegerField(primary_key=True,validators=[MaxValueValidator(999999),MinValueValidator(100000)])
+    truckNumber = models.ForeignKey(AdminTruck, on_delete=models.CASCADE)
+    clientId = models.ForeignKey(Client, on_delete=models.CASCADE)
+    clientTruckId = models.PositiveIntegerField(primary_key=True, validators=[
+                                                MaxValueValidator(999999), MinValueValidator(100000)])
+
     def __str__(self):
-        return  str(self.truckNumber) + str(self.clientId)
-    
-    
+        return str(self.truckNumber) + str(self.clientId)
